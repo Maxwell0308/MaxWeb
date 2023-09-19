2e41a74734a8d8b02f20a606f3e437674fdbf628
Maxwell Hilary <74822038+Maxwell0308@users.noreply.github.com>
2023-09-19 02:40:21 +0000

    Done update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/images/Home.jpeg";
+import image from "./images/Home.jpeg";
 
 const imageAltText = "Cats bro cmon";
 
